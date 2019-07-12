c2d058837710c54350b50e85cf47143025b2cf29
Konstantin Tkachev <sefon85@gmail.com>
2019-07-12 11:24:24 +0800

    fix slow view

--- frontend/web/gm2/js/slowview.js
+++ frontend/web/gm2/js/slowview.js
@@ -1,6 +1,6 @@
 function SlowView()
     {
-	$.alert("zzz");
+	
 	$('#slwvw').fadeOut(1000);
         //$('#nrmvw').fadeIn(1000);
         $('#nrmvw').fadeOut(300);
@@ -22,6 +22,7 @@ function MidleView()
     }
 function LargeView()
     {
+
         $('#slwvw').fadeOut(1000);
         //$('#nrmvw').fadeIn(1000);
         //$('div:not(.lBox)')
@@ -29,22 +30,39 @@ function LargeView()
         //jQuery('*:not("#special-version-controls")').css('font-size','26');
         //jQuery("#all_content").children("*").css('font-size','26');
         jQuery("#mj").css('font-size','40');
-	this.LineFix();
+
+	    LineFix();
     }
 
 function MonoView(){
             jQuery("*").css({'color' : 'white','background':'black'});
             $('body').css('background', "#000000");
+            $('.line4').css({'color' : 'white','background':'black'});
+            $('.line5').css({'color' : 'black','background':'white'});
+            $('.line6').css({'color' : 'white','background':'black'});
+            LineFix();
+            $('.line4').css({'color' : 'white','background':'black'});
         }
 function AlterMonoView(){
             jQuery("*").css({'color' : 'black','background':'white'});
             $('body').css('background', "#FFFFFF");
+            $('.line4').css({'color' : 'black','background':'white'});
+            $('.line6').css({'color' : 'white','background':'black'});
+            LineFix();
         }        
 function LineFix(){
-		$('#line1').css('font-size', '18');
-		$('#line2').css('font-size', '22');
-		$('#line3').css('font-size', '26');
-	}
+        $('.line1').css('font-size', '18');
+        $('.line1').css('background', "#d6d6d6");
+		$('.line2').css('font-size', '22');
+        $('.line2').css('background', "#d6d6d6");
+		$('.line3').css('font-size', '26');
+        $('.line3').css('background', "#d6d6d6");
+        $('.line7').css('background', "#d6d6d6");
+        $('#crfont-style').css('background', "#d6d6d6");
+        $('#special-version-controls').css('background', "#d6d6d6");
+
+        
+        }
 function NormalView()
     {
         //$('#nrmvw').fadeOut(2000);
