f4e04bbdf2aa3c63a344595fb9d791a7b5bcfb38
Konstantin Tkachev <sefon85@gmail.com>
2019-07-23 01:11:41 +0800

    add answers

--- frontend/views/request/index.php
+++ frontend/views/request/index.php
@@ -5,12 +5,16 @@ use yii\grid\GridView;
 use frontend\widgets\commentList\CommentList;
 use yii\helpers\ArrayHelper;
 use app\models\Request;
+use app\models\Answer;
 use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\RequestSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 $rq = Request::find()->all();
 $rqd = ArrayHelper::map($rq, 'subject', 'body', 'name' );
+$rqt = new Answer;
+$ans = $rqt->find(['request_id' => 1])->one();
+//$ans = $rqt->getAnswers()->one();
 //$this->registerJsFile(Yii::$app->request->baseUrl.'/js/ckeditor/ckeditor.js',['depends' => [\yii\web\JqueryAsset::className()]]);
 //$this->registerCssFile("@web/css/message.css", ['depends' => [\yii\bootstrap\BootstrapAsset::className()], 'media' => 'print', ], 'css-print-theme');
 $this->registerCssFile("@web/css/message.css", ['depends' => [\yii\bootstrap\BootstrapAsset::className()]]);
@@ -48,7 +52,7 @@ $this->params['breadcrumbs'][] = $this->title;
             //echo '<H3>' . $rq_name .' ' . $rq_body .' '. $rq_mail  .' '. $rq_date .' ' .'</H3>';
         }
 
-        //print_r($rqd) ;
+    print_r(  $ans) ;
         ?>
         </div>
     </div>
