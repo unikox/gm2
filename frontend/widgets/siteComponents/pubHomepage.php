be391ed6fbdc9e515c1e58c58dcaeb4d24d5ae60
Konstantin Tkachev <sefon85@gmail.com>
2020-09-18 15:19:16 +0800

    fix bugs in show homepages

--- frontend/widgets/siteComponents/pubHomepage.php
+++ frontend/widgets/siteComponents/pubHomepage.php
@@ -10,7 +10,7 @@ class pubHomepage extends Widget
 
     public $HpageBody;
 
-
+    private $blank_state;
     private $startHpage;
     private $stopHpage;
 
@@ -21,17 +21,20 @@ class pubHomepage extends Widget
 
 
 
-		$this->startHpage = '<div class="index-box">';
-        if ($this->HpageBody === null) {
-            $this->HpageBody ='Нет матерьяла для отображения';
+	$this->startHpage = '<div class="index-box">';
+        if (count($this->HpageBody)<1) {
+       	    $this->HpageBody[0]['body'] ='Нет материала для отображения';
+            //$this->HpageBody ='Нет матерьяла для отображения';
         }
-		$this->stopHpage = "</div>";
+	$this->stopHpage = "</div>";
 
 	}
 	public function run()
 	{
         echo $this->startHpage;
-        echo $this->HpageBody[0]['body'];
+        	echo $this->HpageBody[0]['body'];
+		//var_dump( $this->HpageBody);
+	
         //var_dump($this->HpageBody);
         echo $this->stopHpage;
 		return "
