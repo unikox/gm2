924a6076c33408579378fd47b37ed0de9f160f77
Konstantin Tkachev <sefon85@gmail.com>
2021-01-28 16:57:58 +0800

    update news pager for covid

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,11 +1,10 @@
 <?php
 
-use app\models\News;
 use app\models\Hpage;
 use frontend\widgets\siteComponents\pubHomepage;
-use frontend\widgets\siteComponents\pubNews;
 use yii\widgets\ListView;
 
+$this->registerJsFile(Yii::$app->request->baseUrl.'/js/news.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
 /* @var $this yii\web\View */
 $hpage = new Hpage();
 $this->title = 'Гимназия №2';
@@ -14,29 +13,35 @@ $this->title = 'Гимназия №2';
     <?php
     //var_dump($mit->getSections());
     echo pubHomepage::widget([
-        'HpageBody' =>$hpage->getBody(),
+        'HpageBody' => $hpage->getBody(),
  //       'subsections' =>$mit->getSubSections(),
     ]);
     ?>
     <div class="news_pager_box">
         <div class="news_pager_headers_list">
-            <a class='news_pager_headers_item' href="/index.php?r=news" title='Нажмите для перехода в новости'>Новости</a>
-            <a class='news_pager_headers_item' href="/index.php?r=NewsSearch[shortbody]=covid&r=news" title='Нажмите для перехода в новости по COVID-19'>ДОСТОВЕРНО о COVID</a>
+            <p class='news_pager_headers_item'  id='news'>Новости</p>
+            <p class='news_pager_headers_item'   id='news_covid'>ДОСТОВЕРНО о COVID</p>
 
         </div>
-        <div class="news_item_box">
-            <?= ListView::widget([
-                'dataProvider' => $dataNewsProvider,
-                'itemView' => '_news__item',
-                'summary' => false,
-                'viewParams' => [
-                    'fullView' => true,
-                    'context' => 'main-page',
-
-                ],
-            ]);?>
+        <div class="news_item_container">
+            <div class="news_item_box">
+                <?= ListView::widget([
+                    'dataProvider' => $dataNewsProvider,
+                    'itemView' => '_news__item',
+                    'summary' => false,
+                    'viewParams' => [
+                        'fullView' => true,
+                        'context' => 'main-page',
+                    ],
+                ]); ?>
+            </div>
+            <div class="news_item_covid">
+                    <?php
+                        //var_dump($covid_data);
+                        echo $covid_data['page_body'];
+                    ?>
+            </div>
         </div>
-
     </div>
 
 </div>
